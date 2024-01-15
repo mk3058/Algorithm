658f78d8a1e0153bf43c104b01ca24ca4a5c37b0
Minkyu Kim <kmk3058@icloud.com>
2024-01-15 23:57:21 +0900

    [Silver II] Title: 주식, Time: 2560 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.cc
+++ 백준/Silver/11501. 주식/주식.cc
@@ -1,51 +1,60 @@
+#include <climits>
 #include <iostream>
 
 using namespace std;
+
+/**
+ * 경우의 수
+ *  가격이 최고점에 도달하지 않은 경우 -> 주식을 한개 산다
+ *  가격이 최고점에 도달한 경우 -> 주식을 모두 판다
+ *  -> 최적부분구조
+ *
+ *  오늘의 선택이 내일 선택을 제약하는가? -> X -> greedy choice property
+ *
+ *  -> 그리디?
+ */
 
 int price[1000001];
 int totalDays = 0;
 
 int main() {
   int test_case;
-
   cin >> test_case;
 
   for (int t = 0; t < test_case; t++) {
     long long profit = 0;
-    int stockAmount = 0;
 
     cin >> totalDays;
     for (int i = 1; i <= totalDays; i++) {
       cin >> price[i];
     }
+    // 입력
 
     int i = 1;
     while (i <= totalDays) {
-      int max = price[i];
-      int idx = i;
+      int max = INT_MIN;
+      int idx = 0, accumulatedPrice = 0, tmp = 0;
 
-      for (int k = i + 1; k <= totalDays; k++) {
-        if (price[k] == 10000) {
-          idx = k;
-          break;
-        }
+      for (int k = i; k <= totalDays; k++) {
         if (price[k] > max) {
           max = price[k];
           idx = k;
+          accumulatedPrice = tmp;
+          if (max == 10000) {
+            break;
+          }
         }
+        tmp += price[k];
       }
+      // 최댓값 index와 그때의 비용 계산
 
-      for (int j = i; j <= idx - 1; j++) {
-        stockAmount++;
-        profit -= price[j];
+      if (i <= idx - 1) {
+        profit += (((idx - i) * price[idx]) - accumulatedPrice);
       }
-
-      if (stockAmount) {
-        profit += (stockAmount * price[idx]);
-        stockAmount = 0;
-      }
+      // 손익 계산. (판매 - 구매) 누적
       i = idx + 1;
     }
+
     if (profit < 0) {
       profit = 0;
     }
