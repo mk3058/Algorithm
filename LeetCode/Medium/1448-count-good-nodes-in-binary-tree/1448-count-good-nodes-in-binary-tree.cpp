52896f71ed58c84e32ed1bfc02238fdf5b70f92b
Minkyu Kim <kmk3058@icloud.com>
2024-03-26 10:32:07 +0900

    Time: 86 ms (89.77%), Space: 84.7 MB (44.40%) - LeetHub

--- LeetCode/Medium/1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ LeetCode/Medium/1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -13,10 +13,6 @@
 class Solution {
 public:
     int goodNodes(TreeNode* root) {
-        if (!root) {
-            return 0;
-        }
-        
         int goodNodes = 0;
         dfs(root, INT_MIN, goodNodes);
         return goodNodes;
@@ -24,16 +20,15 @@ public:
     
     // depth-first-search by preorder traversal 
     void dfs(TreeNode *node, int max, int &goodNodeCount) {
+        if (!node) {
+            return ;
+        }
+        
         if (node->val >= max) {
             max = node->val;
             goodNodeCount++;
         }
-        
-        if (node->left) {
-            dfs(node->left, max, goodNodeCount);
-        }
-        if (node->right) {
-            dfs(node->right, max, goodNodeCount);
-        }
+        dfs(node->left, max, goodNodeCount);
+        dfs(node->right, max, goodNodeCount);
     }
 };
